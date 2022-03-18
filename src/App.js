c3b3b2022b5a372a9d9611abd842c99d2244e110
Yampier Ponce <developer.yampier@gmail.com>
2022-03-18 16:26:48 -0500

    ✨ diseñando recientes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-// import About from "./components/about";
-import Header from "./components/header";
-import Hero from "./components/hero";
-import Movile from "./components/navigation/movile";
-import Servicios from "./components/servicios";
-import Skills from "./components/skills";
-import Proyect from "./components/proyect";
-import Footer from "./components/footer";
-import Contact from "./components/contact";
-
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Hero />
-      <Movile />
-      {/* <About /> */}
-      <Servicios />
-      {/* <Skills /> */}
-      <Proyect />
-      <Contact />
-      <Footer />
+      <h2>Nuevo comienzo</h2>
     </div>
   );
 }
